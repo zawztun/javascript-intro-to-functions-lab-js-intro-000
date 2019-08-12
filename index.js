ada7c679f8e16b9d8bed5aa02ef0bc2c2351b1f4
Donal tun <americanarakan@gmail.com>
2019-08-12 03:19:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,9 +15,7 @@ function logWhisper(string){
 }
 function sayHiToGrandma(string){
   if (string.toLowerCase)
-  return "I can't hear you!"
-}
-function sayHiToGrandma(string){
+  return "I can't hear you!"}
   if( string.toUpperCase)
   return "YES INDEED"
 }
