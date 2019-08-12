8ad8c5c5b30d5b08e0dee1a85568ee7fd428cfa6
Donal tun <americanarakan@gmail.com>
2019-08-12 03:37:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,4 +18,4 @@ function sayHiToGrandma(string){
   return "I can't hear you!"}
   else if ( string === string.toUpperCase()){
       return "YES INDEED!"
-  }
+  }else if (string === " I love you, Grandma.")
